df95a5e50ae50bf70e5a63a930281a0027b3beaa
Kyiotaro <kyiotaro7@gmail.com>
2026-05-08 10:33:37 +0700

    unity save

--- Assets/Script/Player/PlayerBullet.cs
+++ Assets/Script/Player/PlayerBullet.cs
@@ -11,6 +11,11 @@ public class PlayerBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
