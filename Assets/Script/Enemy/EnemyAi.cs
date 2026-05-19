c0944f64f9836a2bc71a65c7154ddf517fb762ff
Kyiotaro <kyiotaro7@gmail.com>
2026-05-19 11:11:59 +0700

    adjust enemy ai an enmey shoot

--- Assets/Script/Enemy/EnemyAi.cs
+++ Assets/Script/Enemy/EnemyAi.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    public GameObject laserPrefab;
+    public GameObject bulletPrefab;
     private GameObject player;
     public int segment = 10;
     public float space = 1.6f;
@@ -13,11 +13,12 @@ public class EnemyAi : MonoBehaviour
     public float rotationSpeed = 200f;
     private Quaternion targetRotation;
     private Renderer rend;
-    private float shootTimer;
     private Vector2 velocity;
     public float thrustForce = 7f;
     public float damping = 0.99f;
     public float maxSpeed = 8f;
+    private float shootTimer = 0.3f;
+    private float nextShootTime = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,7 +26,6 @@ public class EnemyAi : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         rend = GetComponent<Renderer>();
         speed = 4f;
-        shootTimer = Random.Range(3f, 5f);
     }
 
     // Update is called once per frame
@@ -42,11 +42,10 @@ public class EnemyAi : MonoBehaviour
         }
         if (IsShootingEnabled)
         {
-            shootTimer -= Time.deltaTime;
-            if (shootTimer <= 0)
+            if (transform.rotation == targetRotation && Time.time >= nextShootTime)
             {
-                Laser();
-                shootTimer = Random.Range(3f, 5f);
+                Shoot();
+                nextShootTime = Time.time + shootTimer;
             }
         }
 
@@ -56,17 +55,9 @@ public class EnemyAi : MonoBehaviour
         }
     }
 
-    void Laser()
+    void Shoot()
     {   
-        IsMovementEnabled = false;
-        for (int i = 0; i < segment; i++)
-        {
-            Vector3 pos = transform.position + transform.up * (start +(i * space));
-            pos.z = 0.1f;
-            Instantiate(laserPrefab, pos, transform.rotation);
-        }
-        Invoke("DestroyLaser", 0.2f);
-        Invoke("EnableMovement", 0.3f); // Delay enabling movement slightly
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
     }
 
     void EnableMovement()
@@ -74,14 +65,6 @@ public class EnemyAi : MonoBehaviour
         IsMovementEnabled = true;
     }
 
-    void DestroyLaser()
-    {
-        GameObject[] lassers = GameObject.FindGameObjectsWithTag("Lasser");
-        foreach (GameObject lasser in lassers)
-        {
-            Destroy(lasser);
-        }
-    }
     void Movement()
     {   
         //face the player
