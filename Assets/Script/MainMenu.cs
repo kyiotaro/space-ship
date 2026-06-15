d2a9ef2a8d5306bddc9ef0afde356a9e2e0faf30
Kyiotaro <kyiotaro7@gmail.com>
2026-06-15 10:40:28 +0700

    add Main menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startGame();
+        }
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
